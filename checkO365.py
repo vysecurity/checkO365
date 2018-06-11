4e53c109e5ed1c876e5deb0103098c33a9174780
nma <nma-io@users.noreply.github.com>
2018-06-11 08:14:49 -0600

    Dictionary for easier additions.
    Added a dictionary instead of individual lines for each type. Might make it easier in the future to add more URLs, etc

--- checkO365.py
+++ checkO365.py
@@ -7,59 +7,39 @@ def get_args():
     return parser.parse_args()
 
 def banner():
-	print "Office365 Check Tool"
-	print "Author: Vincent Yiu (@vysecurity)"
-	print "https://www.github.com/vysec/checkO365"
-	print "Version: 1.0"
-	print ""
+    print "Office365 Check Tool"
+    print "Author: Vincent Yiu (@vysecurity)"
+    print "https://www.github.com/vysec/checkO365"
+    print "Version: 1.0"
+    print ""
 
 def main():
-	banner()
-	args = get_args()
-	domain = args.domain
-	domain = domain.replace(".","-")
-	print "Checking: %s" % args.domain
+    banner()
+    args = get_args()
+    domain = args.domain
+    domain = domain.replace(".", "-")
+    print "Checking: %s" % args.domain
 
-	use = False
-	
-	try:
-		host = socket.gethostbyname(domain + ".mail.protection.outlook.com") # your os sends out a dns query
-		print "[*] %s uses Global Office365 and resolves to: %s" % (args.domain,host)
-		print "> " + domain + ".mail.protection.outlook.com"
-		use = True
-	except:
-		pass
-		#print "[!] %s does not use Global Office365" % args.domain
+    use = False
 
-	try:
-		host = socket.gethostbyname(domain + ".mail.protection.outlook.de") # your os sends out a dns query
-		print "[*] %s uses NL Office365 and resolves to: %s" % (args.domain,host)
-		print "> " + domain + ".mail.protection.outlook.de"
-		use = True
-	except:
-		pass
-		#print "[!] %s does not use NL Office365" % args.domain
+    url_dict = {
+        "Global Office365": ".mail.protection.outlook.com",
+        "NL Office365": ".mail.protection.outlook.de",
+        "CN Office 365 (21vianet)": ".mail.protection.partner.outlook.cn",
+        "other convention for Office365": ".mail.protection." + args.domain
+    }
 
-	try:
-		host = socket.gethostbyname(domain + ".mail.protection.partner.outlook.cn") # your os sends out a dns query
-		print "[*] %s uses CN Office365 (21vianet) and resolves to: %s" % (args.domain,host)
-		print "> " + domain + ".mail.protection.partner.outlook.cn"
-		use = True
-	except:
-		pass
-		#print "[!] %s does not use CN Office365" % args.domain
+    for o365 in url_dict:
+        try:
+            host = socket.gethostbyname(domain + url_dict[o365])  # your os sends out a dns query
+            print "[*] %s uses %s and resolves to: %s" % (args.domain, o365, host)
+            print "> " + domain + url_dict[o365]
+            use = True
+        except:
+            pass
 
-	try:
-		host = socket.gethostbyname(domain + ".mail.protection." + args.domain) # your os sends out a dns query
-		print "[*] %s uses other convention for Office365 and resolves to: %s" % (args.domain,host)
-		print "> " + domain + ".mail.protection." + args.domain
-		use = True
-	except:
-		pass
-		#print "[!] %s does not use other Office365" % args.domain
-
-	if not use:
-		print "[!] %s does not seem to use Office365" % args.domain
+    if not use:
+        print "[!] %s does not seem to use Office365" % args.domain
 
 
 if __name__ == '__main__':
